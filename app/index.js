198ee0364a052268a02cdfb85e660540c5ccc8e6
Ubuntu <ubuntu@ip-172-31-84-27.ec2.internal>
2026-04-13 09:17:07 +0000

    Added dynamic Docker image versioning

--- app/index.js
+++ app/index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
   res.send(`
     <body style="background-color:${color}; color:white; text-align:center;">
       <h1>🚀 DevSecOps Demo</h1>
-      <h2>Version: v1.0</h2>
+      <h2>Version: ${process.env.VERSION || "v1"}</h2>
       <h3>Pod: ${os.hostname()}</h3>
     </body>
   `);
